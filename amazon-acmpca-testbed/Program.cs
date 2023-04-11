0f10a497233c628bc9f7eace1b7a616b8d0e8d8e
Mikey Henderson <Michael.Henderson@keyfactor.com>
2023-04-11 09:06:53 -1000

    update manifest for production/public release (#6)
    * update manifest for production/public release
    
    * Update generated README
    
    * Finalize public release
    
    ---------
    
    Co-authored-by: Michael Henderson <mhenderson@keyfactor.com>
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>
    Co-authored-by: David Galey <dgaley@keyfactor.com>

--- amazon-acmpca-testbed/Program.cs
+++ amazon-acmpca-testbed/Program.cs
@@ -16,8 +16,6 @@ namespace amazon_acm_testbed
 
 		//private AmazonCertificateManagerClient getClient()
 		//{
-		//	accessKey = "AKIATGWFWW6RZV7QB4ES";
-		//	accessSecret = "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";
 		//	AmazonACMPCAConfig config = new AmazonACMPCAConfig();
 		//	config.RegionEndpoint = RegionEndpoint.USEast2;
 		//	IAmazonACMPCA client = new AmazonACMPCAClient(accessKey, accessSecret, config);
@@ -44,8 +42,8 @@ namespace amazon_acm_testbed
 		//}
 
 		//ACMPCAConfig config = new ACMPCAConfig();
-		//config.AccessKey = "AKIATGWFWW6RZV7QB4ES";
-		//	config.AccessSecret = "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";
+		//config.AccessKey = accessKey;
+		//	config.AccessSecret = accessSecret;
 		//	config.CAArn = "arn:aws:acm-pca:us-east-2:220531701667:certificate-authority/46916568-f54c-4d7a-8023-accc700a8c98";
 		//	//config.Region = "us-east-2";
 		//	config.S3Bucket = "acmtestbucket";
