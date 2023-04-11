67aaee9a6832d3b5aa05c979ee440c0f038787d8
Dave Galey <89407235+dgaley@users.noreply.github.com>
2023-04-11 14:16:15 -0400

    Update Program.cs

--- amazon-acmpca-testbed/Program.cs
+++ amazon-acmpca-testbed/Program.cs
@@ -12,17 +12,14 @@ namespace amazon_acm_testbed
 	{
 		//private string accessKey;
 		//private string accessSecret;
-		//private string caarn = "arn:aws:acm-pca:us-east-2:220531701667:certificate-authority/46916568-f54c-4d7a-8023-accc700a8c98";
+		//private string caarn;
 
 		//private AmazonCertificateManagerClient getClient()
 		//{
-		//	accessKey = "AKIATGWFWW6RZV7QB4ES";
-		//	accessSecret = "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";
 		//	AmazonACMPCAConfig config = new AmazonACMPCAConfig();
 		//	config.RegionEndpoint = RegionEndpoint.USEast2;
 		//	IAmazonACMPCA client = new AmazonACMPCAClient(accessKey, accessSecret, config);
 		//	AmazonCertificateManagerClient client1 = new AmazonCertificateManagerClient(accessKey, accessSecret, RegionEndpoint.USEast2);
-		//	// test value "arn:aws:acm-pca:us-east-2:220531701667:certificate-authority/46916568-f54c-4d7a-8023-accc700a8c98";
 		//	return client1;
 		//}
 
@@ -44,9 +41,9 @@ namespace amazon_acm_testbed
 		//}
 
 		//ACMPCAConfig config = new ACMPCAConfig();
-		//config.AccessKey = "AKIATGWFWW6RZV7QB4ES";
-		//	config.AccessSecret = "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";
-		//	config.CAArn = "arn:aws:acm-pca:us-east-2:220531701667:certificate-authority/46916568-f54c-4d7a-8023-accc700a8c98";
+		//config.AccessKey = accessKey;
+		//	config.AccessSecret = accessSecret;
+		//	config.CAArn = caarn;
 		//	//config.Region = "us-east-2";
 		//	config.S3Bucket = "acmtestbucket";
 		private static void Main(string[] args)
